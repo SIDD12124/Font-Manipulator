8008a4cd6e953d9bf2028764b4b2b263b76d2da6
SIDD12124 <72179881+SIDD12124@users.noreply.github.com>
2021-06-02 16:16:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    video.position(100, 150)
+
+    canvas = createCanvas(550,550);
+    canvas.position(900,150);
+    poseNet = ml5.poseNet(video, modeloaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
+
+function modeloaded(){
+    console.log('PoseNet Model Is Initialized!')
+}
+
+function draw(){
+    background('#301934');
+}
+
+
+
